0354109ec9fd6ccc35bef4a20b53951694813b51
Evan Waters <evan.waters@gmail.com>
2016-09-16 16:32:52 -0600

    Cleaned up refs to Slowpost

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ var React = require('react');
 var About = React.createClass({
 
   componentDidMount: function() {
-    document.title = "Slowpost";
+    document.title = "Hey!";
   },
 
   render() {
