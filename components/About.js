2859edf1e412621fcd1e08e8baacc6dbc42feee5
Evan Waters <evan.waters@gmail.com>
2016-09-16 16:28:50 -0600

    Not hiding the icon anymore

--- components/About.js
+++ components/About.js
@@ -15,7 +15,7 @@ var About = React.createClass({
           <div className="row">
             <div className="centered col-xs-12 col-sm-12 col-md-12">
               <p className="page-title">Hey! Dance Beats</p>
-              <a href="https://itunes.apple.com/us/app/hey-dance-beats/id1116274610?mt=8"><img className="app-icon hidden-xs hidden-sm" src="assets/hey_logo.png" alt="Slowpost Icon"/></a>
+              <a href="https://itunes.apple.com/us/app/hey-dance-beats/id1116274610?mt=8"><img className="app-icon" src="assets/hey_logo.png" alt="Slowpost Icon"/></a>
               <a href="https://itunes.apple.com/us/app/hey-dance-beats/id1116274610?mt=8"><img className="download-icon" src="assets/Download_on_the_App_Store_Badge_US-UK_135x40.svg" alt="Download on the App Store"/></a>
               <h3>Holler at <a href="https://twitter.com/evanjmwaters" className="link">@evanjmwaters</a> with feeedback and questions</h3>
             </div>
